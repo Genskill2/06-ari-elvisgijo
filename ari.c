490e11d72af87283428282d00fdf874eb375ab02
Elvis Gijo Paul <84615837+elvisgijo@users.noreply.github.com>
2021-06-29 15:59:38 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -2,7 +2,7 @@ char* ari(char* s) {
     int lc = 0;
     int wc = 1;
     int sc = 0;
-    for (int i = 0; i < s[i]!='\0'; i++)
+    for (int i = 0; s[i]!='\0'; i++)
     {
        if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= '0' && s[i] <= '9'))
        {
