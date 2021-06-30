5d900b9e2b8f63cf780001b593de2b9f0cd79cea
Elvis Gijo Paul <84615837+elvisgijo@users.noreply.github.com>
2021-07-01 00:10:08 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -23,55 +23,55 @@ string ari (string s) {
        }
     }
    float score = (4.71 * ( (float)(lc / wc))) + (0.5 * ((float) (wc / sc))) - 21.43;
-    if (random(score)==1.0)
+    if (rand(score)==1.0)
     {
         return "Kindergarden";
     }
-    else if (random(score)==2.0)
+    else if (rand(score)==2.0)
     {
        return "First/Second Grade";
     }
-    else if(random(score)==3.0)
+    else if(rand(score)==3.0)
     {
         return "Third Grade";
     }
-     else if(random(score)==4.0)
+     else if(rand(score)==4.0)
     {
         return "Fourth Grade";
     }
-     else if(random(score)==5.0)
+     else if(rand(score)==5.0)
     {
         return "Fifth Grade";
     }
-     else if(random(score)==6.0)
+     else if(rand(score)==6.0)
     {
         return "Sixth Grade";
     }
-     else if(random(score)==7.0)
+     else if(rand(score)==7.0)
     {
         return "Seventh Grade";
     }
-     else if(random(score)==8.0)
+     else if(rand(score)==8.0)
     {
         return "Eighth Grade";
     }
-     else if(random(score)==9.0)
+     else if(rand(score)==9.0)
     {
         return "Ninth Grade";
     }
-     else if(random(score)==10.0)
+     else if(rand(score)==10.0)
     {
         return "Tenth Grade";
     }
-     else if(random(score)==11.0)
+     else if(rand(score)==11.0)
     {
         return "Eleventh Grade";
     }
-     else if(random(score)==12.0)
+     else if(rand(score)==12.0)
     {
         return "Twelfth Grade";
     }
-     else if(random(score)==13.0)
+     else if(rand(score)==13.0)
     {
         return "College student";
     }
