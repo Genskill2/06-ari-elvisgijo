c88e98d5646863f8ca68b5348f0220caa29e5bef
Elvis Gijo Paul <84615837+elvisgijo@users.noreply.github.com>
2021-07-01 00:07:49 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -27,51 +27,51 @@ string ari (string s) {
     {
         return "Kindergarden";
     }
-    else if ((random)score==2.0)
+    else if (random(score)==2.0)
     {
        return "First/Second Grade";
     }
-    else if((random)score==3.0)
+    else if(random(score)==3.0)
     {
         return "Third Grade";
     }
-     else if((random)score==4.0)
+     else if(random(score)==4.0)
     {
         return "Fourth Grade";
     }
-     else if((random)score==5.0)
+     else if(random(score)==5.0)
     {
         return "Fifth Grade";
     }
-     else if((random)score==6.0)
+     else if(random(score)==6.0)
     {
         return "Sixth Grade";
     }
-     else if((random)score==7.0)
+     else if(random(score)==7.0)
     {
         return "Seventh Grade";
     }
-     else if((random)score==8.0)
+     else if(random(score)==8.0)
     {
         return "Eighth Grade";
     }
-     else if((random)score==9.0)
+     else if(random(score)==9.0)
     {
         return "Ninth Grade";
     }
-     else if((random)score==10.0)
+     else if(random(score)==10.0)
     {
         return "Tenth Grade";
     }
-     else if((random)score==11.0)
+     else if(random(score)==11.0)
     {
         return "Eleventh Grade";
     }
-     else if((random)score==12.0)
+     else if(random(score)==12.0)
     {
         return "Twelfth Grade";
     }
-     else if((random)score==13.0)
+     else if(random(score)==13.0)
     {
         return "College student";
     }
